20d0ebafd649098db2766ca33541f683ad17bcc5
SajanBista <162802857+SajanBista@users.noreply.github.com>
2025-01-10 18:45:48 +0545

    Add files via upload
    numerical method  solutions done

--- main.c
+++ main.c
@@ -1,22 +1,31 @@
-//Trapezoidal rule
-#include<stdio.h>
-//#include<conio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-#define f(x)x+2
-int main(void){
-    printf("Sajan Bista\n");
-    float h,x0,x1,fx0,fx1,v;
-    printf("Enter the lower and upper limit\n");
-    scanf("%f%f",&x0,&x1);
-    h = x1-x0;
-    fx0 = f(x0);
-    fx1=f(x1);
-    v =h/2*(fx0+fx1);
-    printf("value of integration = %f\n",v);
-    //getch();
+#define a(x) (a3*x*x*x + a2*x*x + a1*x + a0) / (-a1)
+
+float a0, a1, a2, a3;
+
+int main(void) {
+    printf("sajan Bista\n fixed point\n");
+    float x0, x1, e, er;
+
+    printf("Enter coefficients a3, a2, a1, and a0: ");
+    scanf("%f %f %f %f", &a3, &a2, &a1, &a0);
+
+    printf("Enter initial guess and E: ");
+    scanf("%f %f", &x0, &e);
+
+    while (1) {
+        x1 = a(x0);
+        er = fabs((x1 - x0) / x1);
+
+        if (er < e) {
+            printf("Root = %f\n", x1);
+            break;
+        }
+
+        x0 = x1;
+    }
+
     return 0;
-    
-    
 }
-
