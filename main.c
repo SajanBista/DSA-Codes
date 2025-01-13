c492a7105e02996f0539547bfd766906c281a51a
SajanBista <162802857+SajanBista@users.noreply.github.com>
2025-01-13 22:34:11 +0545

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,45 @@
-//Trapezoidal rule
-#include<stdio.h>
-//#include<conio.h>
-#include<math.h>
+#include <stdio.h>
 
-#define f(x)x+2
-int main(void){
-    printf("Sajan Bista\n");
-    float h,x0,x1,fx0,fx1,v;
-    printf("Enter the lower and upper limit\n");
-    scanf("%f%f",&x0,&x1);
-    h = x1-x0;
-    fx0 = f(x0);
-    fx1=f(x1);
-    v =h/2*(fx0+fx1);
-    printf("value of integration = %f\n",v);
-    //getch();
+int main() {
+    int n, i, j;
+    float x[100], y[100], a[100], p, v, xv;
+    printf(" sajan bista newton's divided difference\n");
+
+    printf("Enter the number of data points: ");
+    scanf("%d", &n);
+    
+    printf("Enter the value of x for interpolation: ");
+    scanf("%f", &xv);
+    
+    
+    for (i = 0; i < n; i++) {
+        printf("Enter the data points x, and f(x) at i=%d",i);
+        scanf("%f %f", &x[i], &y[i]);
+    }
+
+    
+
+    // Initialize divided difference table
+    for (i = 0; i < n; i++) {
+        a[i] = y[i];
+    }
+
+    // Calculate divided differences
+    for (i = 1; i < n; i++) {
+        for (j = n - 1; j >= i; j--) {
+            a[j] = (a[j] - a[j - 1]) / (x[j] - x[j - i]);
+        }
+    }
+
+    // Calculate interpolated value
+    p = 1;
+    v = a[n - 1];
+    for (i = n - 2; i >= 0; i--) {
+        p *= (xv - x[i]);
+        v += a[i] * p;
+    }
+
+    printf("Interpolated value  = %f\n", v);
+
     return 0;
-    
-    
 }
-
